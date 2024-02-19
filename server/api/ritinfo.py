ff939fcb84144cc8beb2885c360d7855d838428b
ka8540 <ka8540@g.rit.edu>
2024-02-18 19:02:55 -0500

    edit ritinfo_api, ritinfo file

--- server/api/ritinfo.py
+++ server/api/ritinfo.py
@@ -7,49 +7,63 @@ import sys
 import os
 from .swen_610_db_utils import *
 
+# Rebuilds the database tables using the specified SQL script.
+# This could be seen as preparing the 'elements' (tables) for visitors (operations) in the Visitor pattern.
 def rebuild_tables():
     exec_sql_file('infoDetail.sql')
 
+# Retrieves all items from the InfoDetail table.
+# In a Visitor pattern context, this could be considered as gathering all 'elements' for a potential 'visit'.
 def list_info_items():
-    """ Return all items as a list of tuples. """
+    """Fetches all records from the InfoDetail table."""
     result = exec_get_all('''SELECT * FROM InfoDetail''')
     return result
 
+# Inserts a new item into the InfoDetail table and returns its ID.
+# This can be linked to adding a new 'element' that could later be 'visited' by different operations or 'visitors'.
 def insert_info_item(**kwargs):
-    """Insert item into the database and return its ID."""
-    print("Received DATA:", kwargs)
-    firstname = kwargs.get('firstname')
-    lastname = kwargs.get('lastname')
-    email = kwargs.get('email')
-    uid = kwargs.get('uid')
-    mobilenumber = kwargs.get('mobilenumber')
-    sql = '''INSERT INTO infoDetail (firstname, lastname, email, uid, mobileNum) 
-             VALUES (%s, %s, %s, %s, %s) RETURNING id;'''
-    args = (firstname, lastname, email, uid, mobilenumber)
+    """
+    Inserts a new information item into the database.
+    
+    Args:
+        **kwargs: Details of the item to be inserted (firstname, lastname, email, uid, mobilenumber).
+    
+    Returns:
+        The ID of the newly inserted item, allowing for identification in future 'visits'.
+    """
+    sql = '''INSERT INTO infoDetail (firstname, lastname, email, uid, mobileNum) VALUES (%s, %s, %s, %s, %s) RETURNING id;'''
+    args = (kwargs.get('firstname'), kwargs.get('lastname'), kwargs.get('email'), kwargs.get('uid'), kwargs.get('mobilenumber'))
     inserted_id = exec_commit(sql, args)
-    print(inserted_id)
     return inserted_id
 
-
+# Updates an existing item in the InfoDetail table.
+# Conceptually, this could be seen as modifying an 'element' before it receives further 'visits' by operations.
 def update_info_item(**kwargs):
-    """ Update a food item in the database and return its details. """
-    item_id = kwargs.get('item_id')
-    print("id", item_id)
-    firstname = kwargs.get('firstname')
-    lastname = kwargs.get('lastname')
-    email = kwargs.get('email')
-    uid = kwargs.get('uid')
-    mobilenumber = kwargs.get('mobilenumber')
-
-    sql = '''UPDATE InfoDetail SET firstname = %s, lastname = %s ,email = %s, uid = %s, mobileNum=%s
-             WHERE id = %s RETURNING id;'''
-    updated_item = exec_commit(sql, (firstname, lastname ,email, uid, item_id,mobilenumber))
-
-    print(updated_item)
+    """
+    Updates details of an existing information item in the database.
+    
+    Args:
+        **kwargs: New details for the item along with its ID.
+    
+    Returns:
+        The ID of the updated item, ensuring it can still be identified for any 'visits'.
+    """
+    sql = '''UPDATE InfoDetail SET firstname=%s, lastname=%s, email=%s, uid=%s, mobileNum=%s WHERE id=%s RETURNING id;'''
+    args = (kwargs['firstname'], kwargs['lastname'], kwargs['email'], kwargs['uid'], kwargs['mobilenumber'], kwargs['item_id'])
+    updated_item = exec_commit(sql, args)
     return updated_item
 
-
+# Deletes an item from the InfoDetail table.
+# This could be viewed as removing an 'element' from the pool of items that can be 'visited'.
 def delete_info_item(item_id):
-    """ Delete a food item from the database. """
+    """
+    Deletes an information item from the database.
+    
+    Args:
+        item_id: The ID of the item to be deleted.
+    
+    Returns:
+        The result of the delete operation, typically confirmation of deletion.
+    """
     result = exec_commit('''DELETE FROM InfoDetail WHERE id = %s;''', (item_id,))
     return result
--- server/api/ritinfo_api.py
+++ server/api/ritinfo_api.py
@@ -1,45 +1,52 @@
-from flask_restful import Resource, reqparse, request  #NOTE: Import from flask_restful, not python
+# Import necessary Flask extensions and utility functions.
+from flask_restful import Resource, reqparse, request
 from flask import jsonify
-from .swen_610_db_utils import *
-from .ritinfo import *
+from .swen_610_db_utils import *  
+from .ritinfo import * 
 import json
 
-
-# class Init(Resource):
-#     def post(self):
-#         rebuild_tables()
-
-# class Version(Resource):
-#     def get(self):
-#         return (exec_get_one('SELECT VERSION()'))
+# Define an InfoItem class that represents an entity containing user details.
 class InfoItem:
+    # Constructor to initialize an InfoItem object with necessary attributes.
     def __init__(self, firstname, lastname, email, uid, mobilenumber):
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
         self.uid = uid
         self.mobilenumber = mobilenumber
+
+    # Accept method that allows a visitor to perform operations on the InfoItem instance.
+    # This method is part of the Visitor pattern and allows for extending functionality without modifying the class.
     def accept(self, visitor):
         return visitor.visit_info_item(self)
-    
+
+# Abstract base class for visitors that perform operations on InfoItem instances.
 class InfoItemVisitor:
     def visit_info_item(self, info_item):
+        # Placeholder method to be overridden by concrete visitor implementations.
         raise NotImplementedError
-    
+
+# Concrete visitor class for validating InfoItem instances.
 class InfoItemValidator(InfoItemVisitor):
     def visit_info_item(self, info_item):
+        # Perform validation checks on the InfoItem instance.
         errors = []
         if '@' not in info_item.email:
             errors.append('Invalid email address')
+        # Return a list of any found errors.
         return errors
 
+# Resource class for handling CRUD operations on info items via RESTful APIs.
 class InfoDetails(Resource):
+    # Handle GET requests to fetch all info items.
     def get(self):
+        # Utilize a utility function to fetch and return all info items as JSON.
         return jsonify(list_info_items())
 
+    # Handle POST requests to create a new info item.
     def post(self):
+        # Parse incoming JSON data from the request.
         parser = reqparse.RequestParser()
-        parser.add_argument('id', type=str, location='json')
         parser.add_argument('firstname', type=str, required=True, location='json')
         parser.add_argument('lastname', type=str, required=True, location='json')
         parser.add_argument('email', type=str, required=True, location='json')
@@ -47,20 +54,25 @@ class InfoDetails(Resource):
         parser.add_argument('mobilenumber', type=str, required=True, location='json')
         args = parser.parse_args()
 
-        info_item = InfoItem(args['firstname'], args['lastname'], args['email'], args['uid'], args['mobilenumber']) 
+        # Create an InfoItem instance from the parsed arguments.
+        info_item = InfoItem(**args)
 
-        # Validate the info item using the Visitor pattern
+        # Instantiate a validator visitor to validate the info item.
         validator = InfoItemValidator()
+        # Use the accept method to apply validation logic to the info item.
         errors = info_item.accept(validator)
-        print(errors)
-            
 
-        # If validation passes, insert the item
+        # Check if there were any validation errors.
+        if errors:
+            # Return errors as JSON response with HTTP status code 400 (Bad Request).
+            return jsonify({"status": "error", "errors": errors}), 400
+
+        # Insert the info item into the database if validation passes.
         response = insert_info_item(**args)
+        # Return a success response with the inserted info item details.
         return jsonify(response)
 
-
-    
+    # Handle PUT requests to update an existing info item by its ID.
     def put(self, item_id):
         parser = reqparse.RequestParser()
         print("item_id",item_id)
@@ -81,6 +93,6 @@ class InfoDetails(Resource):
             mobilenumber=args['moobilenumber']
         )
         return jsonify(response)
-
+    # Handle DELETE requests to remove an info item by its ID.
     def delete(self, item_id):
         return jsonify(delete_info_item(item_id=item_id))
